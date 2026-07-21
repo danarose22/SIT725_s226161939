8a0a42037fcd9afbf3ecfe6e387f63d68ff8923d
danarose22 <s226161939@deakin.edu.au>
2026-07-21 21:50:00 +1000

    Task2

--- week2/server.js
+++ week2/server.js
@@ -4,18 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve the index.html file from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// GET API to add two numbers
-// Example: http://localhost:3000/add?a=10&b=20
 app.get('/add', (req, res) => {
     const a = parseFloat(req.query.a);
     const b = parseFloat(req.query.b);
 
     if (isNaN(a) || isNaN(b)) {
         return res.status(400).send(
-            "Please provide two valid numbers. Example: /add?a=10&b=20"
+            "Please provide two valid numbers."
         );
     }
 
@@ -28,7 +25,6 @@ app.get('/add', (req, res) => {
     });
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
